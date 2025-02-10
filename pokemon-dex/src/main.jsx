43225a65671dcf926781e3745e94ce36d7af3aac
sohxxny <sohxxny@gmail.com>
2025-02-11 01:14:14 +0900

    feat: 로컬 스토리지로 데이터 유지 구현
    변경사항:
    - redux-persist 설치
    - configureStore.js 에 persist 추가
    - main.jsx에 PersistGate 적용
    
    관련 이슈: #23

--- pokemon-dex/src/main.jsx
+++ pokemon-dex/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/config/configStore";
+import { persistor, store } from "./redux/config/configStore";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- pokemon-dex/src/redux/config/configStore.js
+++ pokemon-dex/src/redux/config/configStore.js
@@ -1,8 +1,36 @@
 import pokemonReducer from "../slices/pokemonSlice";
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+
+const rootReducer = combineReducers({
+  pokemonReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    pokemonReducer: pokemonReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
